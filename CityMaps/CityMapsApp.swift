f740d630504e2410d7a7b966aecd7d2d94c938f7
Martín Valorzi <martin.valorzi@bancogalicia.com.ar>
2024-08-21 22:58:42 -0300

    Initial Commit

---
+++ CityMaps/CityMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityMapsApp.swift
+//  CityMaps
+//
+//  Created by Martin Valorzi on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CityMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
